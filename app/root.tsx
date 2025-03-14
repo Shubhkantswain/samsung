a1497586fc0e837c05eecd3454b006eeb359a512
shubhswain45 <shubhkantswain@.com>
2025-03-14 20:08:23 +0530

    ttrrtt

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-[#212121]">
         {children}
         <ScrollRestoration />
         <Scripts />
